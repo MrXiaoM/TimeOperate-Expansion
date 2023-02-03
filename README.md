854b7d10ab68495895264e8a0bd0dfcb226035df
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-02-03 13:59:11 +0800

    Updated 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # Time-Expansion
 Time operating expansion for PlaceholderAPI
+
+# Install
+
+* Download jar file from [releases](https://github.com/MrXiaoM/Time-Expansion/releases).
+* Put the jar file to `/plugins/PlaceholderAPI/expansions/`
+* Execute command `/papi register <filename>` or `/papi reload` or restart server.
+
+# Usage
+
+```
+// Arguments: <Required> [Optional]
+
+%time_<time>_<format>[_<time override>]%
+```
+Type `{placeholder}` or `(placeholder)` if you want to use some placeholders in arguments.
+
+> I am not a native English speaker. There is maybe some typo error.
+
+## Time
+| value          | detail | example                |
+|----------------| --- |------------------------|
+| now           | now time | `now`                  |
+| timestamp     | timestamp (in seconds) | `1675353600`           |
+| DateTimeFormatter&#124;value | specific format time | yyyy-MM-dd&#124;2023-02-03 |
+
+## Format
+
+Use `unix` for returning timestamp (in seconds).
+
+Or read the [DateTimeFormatter](http://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html) Javadoc page about possible formats.
+
+## Time Override
+| value | detail | example |
+| --- | --- | --- |
+| +DURATION | plus time, needed number and unit (ignore case), `d` means `day(s)`, `h` means `hour(s)`, `m` means `minute(s)`, `s` means `second(s)` | `+1d2h` |
+| -DURATION | minus time, needed number and unit (ignore case), `d` means `day(s)`, `h` means `hour(s)`, `m` means `minute(s)`, `s` means `second(s)` | `-1d2h` |
+| unit=value | set specific time | `y=2023` |
+
+Support multi overrides, connect them with `_`.
+
+### Specific Time Unit
+| value | detail |
+| --- | --- |
+| `y`, `year` | Year |
+| `M`, `month` | Month |
+| `d`, `day` | Day |
+| `h`, `hour` | Hour |
+| `m`, `minute` | Minute |
+| `s`, `second` | Second |
---
+++ src/main/java/top/mrxiaom/papi/TimeExpansion.java
@@ -0,0 +1,116 @@
+package top.mrxiaom.papi;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class TimeExpansion extends PlaceholderExpansion {
+    Pattern ROUND_BRACKET_PLACEHOLDER_PATTERN = Pattern.compile("[(]([^()]+)[)]");
+    @Override
+    public String getIdentifier() {
+        return "time";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "MrXiaoM";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer p, String params) {
+        params = PlaceholderAPI.setPlaceholders(p, params, ROUND_BRACKET_PLACEHOLDER_PATTERN);
+        params = PlaceholderAPI.setBracketPlaceholders(p, params);
+        String[] args = params.split("_");
+        if (args.length >= 2) {
+            try {
+                LocalDateTime time = parse(args[0]);
+                if (time == null) return null;
+                time = override(time, args, 2);
+                return format(time, args[1]);
+            } catch (Throwable ignored) {
+            }
+        }
+        return null;
+    }
+
+    public static LocalDateTime parse(String param) {
+        if (param.equalsIgnoreCase("now")) return LocalDateTime.now();
+        if (param.contains("|")) {
+            String[] args = param.split("\\|", 2);
+            DateTimeFormatter format = DateTimeFormatter.ofPattern(args[0]);
+            return LocalDateTime.parse(args[1], format);
+        }
+        long timestamp = Long.parseLong(param);
+        return Instant.ofEpochSecond(timestamp)
+                .atZone(ZoneOffset.ofHours(8))
+                .toLocalDateTime();
+    }
+
+    public static String format(LocalDateTime time, String param) {
+        if (param.equalsIgnoreCase("unix")) return String.valueOf(
+                time.toInstant(ZoneOffset.ofHours(8)).getEpochSecond()
+        );
+        DateTimeFormatter format = DateTimeFormatter.ofPattern(param);
+        return time.format(format);
+    }
+
+    public static LocalDateTime override(LocalDateTime time, String[] args, int startIndex) {
+        for (int i = startIndex; i < args.length; i++) {
+            String override = args[i];
+            if (override.startsWith("+")) {
+                Duration d = parseDuration(override.substring(1));
+                if (d != null) time = time.plusSeconds(d.getSeconds());
+            }
+            else if (override.startsWith("-")) {
+                Duration d = parseDuration(override.substring(1));
+                if (d != null) time = time.minusSeconds(d.getSeconds());
+            }
+            else if (override.contains("=")) {
+                String[] specific = override.split("=", 2);
+                String unit = specific[0];
+                try {
+                    int value = Integer.parseInt(specific[1]);
+                    if (e(unit, "s", "second")) time = time.withSecond(value);
+                    if (e(unit, "m", "minute")) time = time.withMinute(value);
+                    if (e(unit, "h", "hour")) time = time.withHour(value);
+                    if (e(unit, "d", "day")) time = time.withDayOfMonth(value);
+                    if (e(unit, "M", "month")) time = time.withMonth(value);
+                    if (e(unit, "y", "year")) time = time.withYear(value);
+                } catch (NumberFormatException ignored) {
+                }
+            }
+        }
+        return time;
+    }
+
+    public static Duration parseDuration(String s) {
+        try {
+            return Duration.parse("P" + s.substring(1).toUpperCase());
+        } catch (DateTimeParseException ignored){
+            return null;
+        }
+    }
+
+    /**
+     * is equals one of the strings
+     * @param s string
+     * @param a another strings
+     */
+    static boolean e(String s, String... a) {
+        return Arrays.asList(a).contains(s);
+    }
+}
